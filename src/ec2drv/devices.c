0bc00cd6d68e5ed684aa39a169bf38e95130a3fc
Cjacker <jianzhong.huang@i-soft.com.cn>
2022-06-24 11:15:27 +0800

    Drop unused getDevice function

--- src/ec2drv/devices.c
+++ src/ec2drv/devices.c
@@ -4,23 +4,7 @@
 static DEVICE unknown_dev = { "Unknown",-1,255,AUTO,0,8196,512,FALSE };
 extern DEVICE devices[];
 
-// Pick the closest device and return it.
-DEVICE *getDevice( uint8_t id, uint8_t rev )
-{
-	int i = 0;
-	//printf("id=0x%02x, rev=%02x\n",id, rev);
-	do
-	{
-		if( (devices[i].id==id)&&(devices[i].rev==255) )
-			return &devices[i];
-		else if( (devices[i].id==id)&&(devices[i].rev==rev) )
-			return &devices[i];
-	} while( devices[++i].name[0] != 0 );
-	
-	return &unknown_dev;
-}
-
-
+// Pick up the device exactly match thedevice id and derivative_id
 DEVICE *getDeviceByIDAndDerivativeID( uint8_t id, uint8_t derivative_id, uint8_t rev)
 {
 	int i=0;
